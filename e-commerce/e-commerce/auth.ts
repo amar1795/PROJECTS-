2186b6de339ff5cd1135aca9ae6e4e9fecc296af
Amar <jeetamar496@gmail.com>
2024-05-21 18:48:52 +0530

    actions created next auth atuthentication added

--- e-commerce/e-commerce/auth.ts
+++ e-commerce/e-commerce/auth.ts
@@ -4,15 +4,6 @@ import Credentials from 'next-auth/providers/credentials';
 import { z } from 'zod';
 
 
-async function getUser(email: string): Promise<User | undefined> {
-    try {
-      const user = await sql<User>`SELECT * FROM users WHERE email=${email}`;
-      return user.rows[0];
-    } catch (error) {
-      console.error('Failed to fetch user:', error);
-      throw new Error('Failed to fetch user.');
-    }
-  }
 export const { auth, signIn, signOut } = NextAuth({
     ...authConfig,
     providers: [Credentials({
@@ -21,11 +12,7 @@ export const { auth, signIn, signOut } = NextAuth({
                 .object({ email: z.string().email(), password: z.string().min(6) })
                 .safeParse(credentials);
 
-                if (parsedCredentials.success) {
-                    const { email, password } = parsedCredentials.data;
-                    const user = await getUser(email);
-                    if (!user) return null;
-                  }
+                
             return null;
         },
     }),],
