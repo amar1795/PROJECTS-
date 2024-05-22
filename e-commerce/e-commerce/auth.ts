10ef1d12a23d172266550d8626aee3f46da37d8f
Amar <jeetamar496@gmail.com>
2024-05-22 23:17:43 +0530

    next auth credentials login logic written

--- e-commerce/e-commerce/auth.ts
+++ e-commerce/e-commerce/auth.ts
@@ -2,19 +2,52 @@ import NextAuth from 'next-auth';
 import { authConfig } from './auth.config';
 import Credentials from 'next-auth/providers/credentials';
 import { z } from 'zod';
+import { db } from "@/lib/db";
+import bcrypt from "bcryptjs";
 
+export const LoginSchema = z.object({
+    email: z.string().email({
+      message: "Email is required",
+    }),
+    password: z.string().min(1, {
+      message: "Password is required",
+    }),
+    code: z.optional(z.string()),
+  });
+  
+  export const getUserByEmail = async (email: string) => {
+    try {
+      const user = await db.user.findUnique({ where: { email } });
+  
+      return user;
+    } catch {
+      return null;
+    }
+  };
 
 export const { auth, signIn, signOut } = NextAuth({
     ...authConfig,
-    providers: [Credentials({
-        async authorize(credentials) {
-            const parsedCredentials = z
-                .object({ email: z.string().email(), password: z.string().min(6) })
-                .safeParse(credentials);
-
+    providers: [
+        Credentials({
+            async authorize(credentials) {
+              
+              const validatedFields = LoginSchema.safeParse(credentials);
+              if (validatedFields.success) {
+                const { email, password } = validatedFields.data;
                 
-            return null;
-        },
-    }),],
+                const user = await getUserByEmail(email);
+                if (!user || !user.password) return null;
+      
+                const passwordsMatch = await bcrypt.compare(
+                  password,
+                  user.password,
+                );
+      
+                if (passwordsMatch) return user;
+              }
+      
+              return null;
+            }
+          })],
 
 });
