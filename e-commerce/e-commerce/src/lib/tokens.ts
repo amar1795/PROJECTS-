abdee86d46b9f3b47fe1026f7712d1e97b6a33e4
Amar <jeetamar496@gmail.com>
2024-05-22 23:12:58 +0530

    password reset logic written

--- e-commerce/e-commerce/src/lib/tokens.ts
+++ e-commerce/e-commerce/src/lib/tokens.ts
@@ -8,6 +8,7 @@ export const generateTwoFactorToken = async (email: string) => {
 
   // this line generates a random 6 digit number
   const token = crypto.randomInt(100_000, 1_000_000).toString();
+
   const expires = new Date(new Date().getTime() + 5 * 60 * 1000);
 
    const getTwoFactorTokenByEmail = async (email: string) => {
@@ -32,13 +33,57 @@ export const generateTwoFactorToken = async (email: string) => {
     });
   }
 
-  const twoFactorToken = await db.twoFactorToken.create({
-    data: {
-      email,
-      token,
-      expires,
-    }
-  });
-
-  return twoFactorToken;
+  try {
+    const twoFactorToken = await db.twoFactorToken.create({
+        data: {
+          email,
+          token,
+          expires,
+        }
+      });
+    
+      return twoFactorToken;
+    
+  } catch (error) {
+    console.error('Error creating token:', error);
+    throw error;
+  }
+ 
 }
+
+export const generatePasswordResetToken = async (email: string) => {
+    const token = uuidv4();
+    const expires = new Date(new Date().getTime() + 3600 * 1000);
+
+     const getPasswordResetTokenByEmail = async (email: string) => {
+        try {
+          const passwordResetToken = await db.passwordResetToken.findFirst({
+            where: { email }
+          });
+      
+          return passwordResetToken;
+        } catch {
+          return null;
+        }
+      };
+  
+    const existingToken = await getPasswordResetTokenByEmail(email);
+  
+    if (existingToken) {
+      await db.passwordResetToken.delete({
+        where: { id: existingToken.id }
+      });
+    }
+  
+    const passwordResetToken = await db.passwordResetToken.create({
+      data: {
+        email,
+        token,
+        expires
+      }
+    });
+  
+    return passwordResetToken;
+  
+    
+  }
