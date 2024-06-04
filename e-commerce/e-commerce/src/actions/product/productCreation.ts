31989bc62aeabd31966db0cb8dac76201f107d0e
Amar <jeetamar496@gmail.com>
2024-06-04 13:22:06 +0530

    5 kids sandals added to the DB

--- e-commerce/e-commerce/src/actions/product/productCreation.ts
+++ e-commerce/e-commerce/src/actions/product/productCreation.ts
@@ -11,20 +11,18 @@ interface ProductParams {
     productBrandId: string;
 }
 const productParams: ProductParams = {
-    productName: " Peppa Paradise Kids' Flip Flops: Splash into Fun with Peppa Pig",
-    productPrice: 8000.00,
-    productBrandId: "665ac7555788e185779d7cdc",
-    productCategoryId: "665df2e246b36eef0288e432",
-    productDiscountPercentage: 22,
-    productDescription: "Let your little one splash into fun with our Peppa Paradise Kids' Flip Flops, featuring adorable Peppa Pig prints that are sure to bring a smile to their face. These cute flip flops showcase colorful graphics of Peppa and her friends, adding a playful touch to their summer style. Made from lightweight and durable material, they offer comfort and support for all-day wear. The textured footbed and non-slip sole provide traction and stability on wet surfaces, while the easy slip-on style makes them convenient for kids to wear. Whether they're playing at the beach or lounging by the pool, these Peppa Pig flip flops will keep their feet happy and stylish all summer long.",
+    productName: "Ocean Breeze Kids' Blue Crocs-Style Sandals: Dive into Comfort and Style",
+    productPrice: 9500.00,
+    productBrandId: "665ac7555788e185779d7cdb",
+    productCategoryId: "665df2e246b36eef0288e433",
+    productDiscountPercentage: 35,
+    productDescription: "Dive into comfort and style with our Ocean Breeze Kids' Blue Crocs-Style Sandals, designed for young adventurers who love to make a splash. These vibrant blue sandals feature a Crocs-inspired design with rectangular perforations along the upper, adding a playful touch to their look. Made from lightweight and durable material, they offer all-day comfort and support for active little feet. The adjustable strap ensures a secure fit, while the textured footbed provides traction and stability on various surfaces. Whether they're exploring tide pools or playing in the backyard, these blue Crocs-like sandals will keep their feet happy and stylish on every adventure.",
     productImages: [
-        "https://res.cloudinary.com/dfveswqdm/image/upload/v1717067048/Kids/Shoes/flip%20flops/71RV8sA0ENL._SY695__pakioo.jpg",
-        "https://res.cloudinary.com/dfveswqdm/image/upload/v1717067036/Kids/Shoes/flip%20flops/71cipHN864L._SY695__sbvjc1.jpg",
-        "https://res.cloudinary.com/dfveswqdm/image/upload/v1717067052/Kids/Shoes/flip%20flops/71UAU-7r7kL._SY695__r6vvkm.jpg",
-        "https://res.cloudinary.com/dfveswqdm/image/upload/v1717067045/Kids/Shoes/flip%20flops/71nsWDY7RRL._SY695__q531cq.jpg",
-        "https://res.cloudinary.com/dfveswqdm/image/upload/v1717067023/Kids/Shoes/flip%20flops/61sAsaIrHZL._SY695__xbd4c8.jpg",
-
-          ]
+        "https://res.cloudinary.com/dfveswqdm/image/upload/v1717067102/Kids/Shoes/sandals/51OhRzDUlLL._SY695__wwzgli.jpg",
+        "https://res.cloudinary.com/dfveswqdm/image/upload/v1717067106/Kids/Shoes/sandals/51TRTbR1-6L._SY695__rz1bbq.jpg",
+        "https://res.cloudinary.com/dfveswqdm/image/upload/v1717067111/Kids/Shoes/sandals/61DoprqdwnL._SY695__air4zy.jpg",
+        "https://res.cloudinary.com/dfveswqdm/image/upload/v1717067097/Kids/Shoes/sandals/51JRkKyn7RL._SY695__lkldut.jpg",
+        "https://res.cloudinary.com/dfveswqdm/image/upload/v1717067100/Kids/Shoes/sandals/51mi_CjhkXL._SY695__bnxjun.jpg", ]
 };
 
 export async function productCreationNew() {
