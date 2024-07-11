4c53122bee84c5888ca1c6e21d4b8d2d4cf7a986
Amar <jeetamar496@gmail.com>
2024-07-11 22:25:34 +0530

    default removed from like api POST request

--- e-commerce/e-commerce/src/app/api/likes/route.ts
+++ e-commerce/e-commerce/src/app/api/likes/route.ts
@@ -2,7 +2,7 @@ import { prismadb } from "@/lib/db";
 import { NextApiRequest, NextApiResponse } from 'next';
 
 
-export default async function POST(req: NextApiRequest, res: NextApiResponse) {
+export async function POST(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method Not Allowed' });
   }
