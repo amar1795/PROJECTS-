c38ab7a4d1629bcf749fab23e3a654bbf5ad7ce6
Amar <jeetamar496@gmail.com>
2024-07-11 22:08:18 +0530

    like api  route handler corrected to POST

--- e-commerce/e-commerce/src/app/api/likes/route.ts
+++ e-commerce/e-commerce/src/app/api/likes/route.ts
@@ -2,7 +2,7 @@ import { prismadb } from "@/lib/db";
 import { NextApiRequest, NextApiResponse } from 'next';
 
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function POST(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method Not Allowed' });
   }
