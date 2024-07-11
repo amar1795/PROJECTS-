da0f844f2d24d712f59755bda02c57a3467bbd6b
Amar <jeetamar496@gmail.com>
2024-07-11 22:35:31 +0530

    ignored typescript error as well , just for basic deployment

--- e-commerce/e-commerce/next.config.js
+++ e-commerce/e-commerce/next.config.js
@@ -8,5 +8,12 @@ module.exports = {
         // your project has ESLint errors.
         ignoreDuringBuilds: true,
       },
+      typescript: {
+        // !! WARN !!
+        // Dangerously allow production builds to successfully complete even if
+        // your project has type errors.
+        // !! WARN !!
+        ignoreBuildErrors: true,
+      },
 }
 
