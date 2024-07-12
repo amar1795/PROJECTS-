9a508be8b311245def108416b03e1af1390a9604
Amar <jeetamar496@gmail.com>
2024-07-12 13:22:30 +0530

    import {PrismaClient} from "../prisma/generated/client"; added in db.ts

--- e-commerce/e-commerce/src/lib/db.ts
+++ e-commerce/e-commerce/src/lib/db.ts
@@ -1,4 +1,6 @@
-import { PrismaClient } from "@prisma/client";
+// import { PrismaClient } from "@prisma/client";
+// import { PrismaClient } from '../../prisma/generated/client';
+import {PrismaClient} from "../prisma/generated/client";
 
 declare global {
   var prisma: PrismaClient | undefined;
