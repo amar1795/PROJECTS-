ce321dc1293ce5be0a3f6cb45bcd263e909eb5af
Amar <jeetamar496@gmail.com>
2024-07-12 12:59:45 +0530

    removed global this from db.ts as well

--- e-commerce/e-commerce/src/lib/db.ts
+++ e-commerce/e-commerce/src/lib/db.ts
@@ -8,4 +8,4 @@ export const prismadb = globalThis.prisma || new PrismaClient();
 
 // if (process.env.NODE_ENV !== "production")
   
-  globalThis.prisma = prismadb;
+  // globalThis.prisma = prismadb;
