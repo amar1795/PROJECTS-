c9784ae30da0c03fc25f0663eb1cb97e0cf5cb6f
Amar <jeetamar496@gmail.com>
2024-07-21 17:43:32 +0530

    next.config.js issue corrected

--- e-commerce/e-commerce/next.config.js
+++ e-commerce/e-commerce/next.config.js
@@ -18,5 +18,7 @@ module.exports = {
       experimental: {
         missingSuspenseWithCSRBailout: false,
       },
+      swcMinify: false,
+
 }
 
--- e-commerce/e-commerce/src/middleware.ts
+++ e-commerce/e-commerce/src/middleware.ts
@@ -1,5 +1,5 @@
 import NextAuth from "next-auth";
-import authConfig from "./auth.config";
+import authConfig from "@/auth.config";
 import { NextApiRequest, NextApiResponse } from "next";
 
 export const publicRoutes = [
